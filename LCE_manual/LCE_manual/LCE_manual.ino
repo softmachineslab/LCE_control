95f4819dfd9babd8c54ec6cd8b8df43cfc2e248f
Andrew Sabelhaus <apsabelhaus@cmu.edu>
2021-07-27 11:27:27 -0400

    Manual control of the LCE actuator. Really, this just sets one pin or another to high/low.

---
+++ LCE_manual/LCE_manual/LCE_manual.ino
@@ -0,0 +1,64 @@
+/*
+ * Manual control of Mason's LCE actuator.
+ * Copyright Soft Machines Lab 2021.
+ * Andrew Sabelhaus and Mason Zadan
+ */
+
+// pin setup and constants
+// switch between fwd, rev, off
+int status = 0;
+int fwd_pin = 3;
+int rev_pin = 5;
+
+void setup() {
+  // Serial is the programming port on the Zero
+  int serial_rate = 115200;
+  Serial.begin(serial_rate);
+  // Wait until the port is ready
+  while (!Serial)
+    ;
+  Serial.println("LCE Control - manual. Specify 0 = off, 1 = forward, 2 = reverse");
+  Serial.println("NOTE! You MUST set the serial terminal to 'No Line Ending', otherwise will reset to 0 immediately.");
+  // ensure we start with all transistors off
+  pinMode(fwd_pin, OUTPUT);
+  pinMode(rev_pin, OUTPUT);
+  digitalWrite(fwd_pin, LOW);
+  digitalWrite(rev_pin, LOW);
+  // we'll also turn on the LED when any pin is set high
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop() {
+  // poll the serial port (wait until user sends something)
+  if( Serial.available() > 0){
+    // presummably the user is smart enough to have only sent one integer...
+    int cmd = Serial.read();
+    // remove the ASCII character offset for numerical digits
+    status = cmd - '0';
+    switch(status){
+      case 0:
+        Serial.println("Status = 0, Off");
+        digitalWrite(fwd_pin, LOW);
+        digitalWrite(rev_pin, LOW);
+        digitalWrite(LED_BUILTIN, LOW);
+        break;
+      case 1:
+        Serial.println("Status = 1, Forward");
+        digitalWrite(fwd_pin, HIGH);
+        digitalWrite(rev_pin, LOW);
+        digitalWrite(LED_BUILTIN, HIGH);
+        break;
+      case 2:
+        Serial.println("Status = 2, Reverse");
+        digitalWrite(fwd_pin, LOW);
+        digitalWrite(rev_pin, HIGH);
+        digitalWrite(LED_BUILTIN, HIGH);
+        break;
+      default:
+        Serial.println("Error! You sent, in ASCII:");
+        Serial.println(cmd);
+        Serial.println("...which is not a valid command. Not changing any pins.");
+    }
+  }
+}
